40c1a7d730e1fb3c64023ed880f89def0a6e7d2a
Wallace Caetano <wallace.santos@youtan.com.br>
2024-06-12 16:41:42 -0300

    estrutura de pastas

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,14 @@
+import {  } from "react-router-dom"
+import { BrowserRouter, Routes as Switch, Route, Navigate } from "react-router-dom"
+import { Dashboard } from "../pages"
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/pagina-inicial" element={<Dashboard />}/>
+        <Route path="*" element={<Navigate to="pagina-inicial"/>}/>
+      </Switch>
+    </BrowserRouter>
+  )
+}
